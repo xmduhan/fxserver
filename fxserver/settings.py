5a984e6c3b4b8b89489e2348370e145251501f4e
xmduhan <xmduhan@gmail.com>
2013-11-30 08:56:48 +0800

    deploy to apache http server

--- fxserver/settings.py
+++ fxserver/settings.py
@@ -50,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/duhan/pydev/fxserver/media'
+MEDIA_ROOT = '/var/fxserver/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/duhan/pydev/fxserver/static'
+STATIC_ROOT = '/var/fxserver/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
